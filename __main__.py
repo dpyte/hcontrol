979812d85f79d68668631c69463ac11859f06186
Dhruv Patel <pateldrv456@gmail.com>
2021-10-23 01:17:36 -0400

    some stuff

--- __main__.py
+++ __main__.py
@@ -1,52 +1,97 @@
 #!/bin/env python
-from collections import deque
 
 import cv2
-import numpy as np
+import mediapipe as mp
+from tracking import coordinates as coo
+from tracking.points import Points
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+location = coo.HandLocation()
+
+DEBUG = True
+
+"""
+ Idea is to having an asynchronous thread that reads off the data from the
+ init function and acts based on the collected data
+"""
+
+
+"""
+	0: Wrist							1: THUMB_CMC					2: THUMB_IP
+	3: THUMB_MCP					4: THUMB_TIP					5: INDEX_FINGER_MCP
+	6: INDEX_FINGER_PIP		7: INDEX_FINGER_DIP		8: INDEX_FINGER_TIP
+	9: MIDDLE_FINGER_MCP	10: MIDDLE_FINGER_PIP	11: MIDDLE_FINGER_DIP
+	12: MIDDLE_FINGER_TIP	13: RING_FINGER_MCP		14: RING_FINGER_PIP
+	15: RING_FINGER_DIP		14: RING_FINGER_TIP		17: PINKY_MCP
+	18: PINKY_PIP					19: PINKY_DIP					20: PINKY_TIP
+"""
+
+
+def extract_coordinates_from_hand_landmark(hands, lm, i_width, i_height):
+		for point in hands.HandLandmark:
+				normalize = lm.landmark[point]
+				coordinated = mp_drawing._normalized_to_pixel_coordinates(
+						normalize.x,
+						normalize.y,
+						i_width,
+						i_height
+				)
+				if DEBUG:
+						if point == 0:
+								print('Point: {}\nCoordinates: {}\nLandmarks:\n{}'.format(
+										point,
+										coordinated,
+										normalize)
+								)
+				# X -> Left & Right
+				# Y -> Down & Up
+				# Z -> Back & foreword
+				x_cord, y_cord, z_cord = normalize.x, normalize.y , normalize.z
+				location.update_value(Points(point), coordinated, x_cord, y_cord, z_cord)
 
 
 def init():
-	cap = cv2.VideoCapture(0)
-	pts = deque(maxlen=64)
-	Lower_green = np.array([110, 50, 50])
-	Upper_green = np.array([130, 255, 255])
-	while True:
-		ret, img = cap.read()
-		hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-		kernel = np.ones((5, 5), np.uint8)
-		mask = cv2.inRange(hsv, Lower_green, Upper_green)
-		mask = cv2.erode(mask, kernel, iterations=2)
-		mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-		# mask=cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel)
-		mask = cv2.dilate(mask, kernel, iterations=1)
-		res = cv2.bitwise_and(img, img, mask=mask)
-		cnts, heir = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
-		center = None
-		if len(cnts) > 0:
-			c = max(cnts, key=cv2.contourArea)
-			((x, y), radius) = cv2.minEnclosingCircle(c)
-			M = cv2.moments(c)
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-			if radius > 5:
-				cv2.circle(img, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-				cv2.circle(img, center, 5, (0, 0, 255), -1)
-		pts.appendleft(center)
-		for i in range(1, len(pts)):
-			if pts[i - 1] is None or pts[i] is None:
-				continue
-			thick = int(np.sqrt(len(pts) / float(i + 1)) * 2.5)
-			cv2.line(img, pts[i - 1], pts[i], (0, 0, 225), thick)
-		cv2.imshow("Frame", img)
-		cv2.imshow("mask", mask)
-		cv2.imshow("res", res)
-		k = cv2.waitKey(30) & 0xFF
-		if k == 32:
-			break
-	# cleanup the camera and close any open windows
-	cap.release()
-	cv2.destroyAllWindows()
+		cap = cv2.VideoCapture(0)
+		with mp_hands.Hands(
+				min_detection_confidence=0.9,
+				min_tracking_confidence=0.5
+				) as hands:
+				while cap.isOpened():
+						succ, img = cap.read()
+						if not succ:
+								print('ignoring empty camera frame')
+								continue
+						img.flags.writeable = False
+						img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+						results = hands.process(img)
+
+						height, width, _ = img.shape
+						img.flags.writeable = True
+						img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+						if results.multi_hand_landmarks:
+								for hl in results.multi_hand_landmarks:
+										extract_coordinates_from_hand_landmark(
+												mp_hands,
+												hl,
+												width,
+												height
+										)
+										mp_drawing.draw_landmarks(
+												img,
+												hl,
+												mp_hands.HAND_CONNECTIONS,
+												mp_drawing_styles.get_default_hand_landmarks_style(),
+												mp_drawing_styles.get_default_hand_connections_style()
+										)
+						if DEBUG:
+								cv2.imshow('HC Hands', cv2.flip(img, 1))
+						if cv2.waitKey(5) & 0xFF == 27:
+								break
+		cap.release()
 
 
 if __name__ == '__main__':
-	init()
-
+		init()
